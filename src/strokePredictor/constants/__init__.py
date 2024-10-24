c666fb87292f9ca7c3cbd83f5ff0d34763d433a1
Sharan-vj <sharanvj678@gmail.com>
2024-10-24 16:08:41 +0530

    feat(constant): updated constant variables

---
+++ src/strokePredictor/constants/__init__.py
@@ -0,0 +1,7 @@
+# Import Dependencies
+from pathlib import Path
+
+# Constant Path Variables
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
